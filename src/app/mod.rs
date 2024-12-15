e5606c8715e4053ce6a185483203d1e7ba74125e
lsy969999 <lsy969999@gmail.com>
2024-12-15 13:20:24 +0900

    rapier

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,13 +1,13 @@
 use crate::{asset::AssetsPlugin, ffi::FfiPlugin, game::GamePlugin, ui::MyUiPlugin};
-use avian3d::prelude::*;
+// use avian3d::prelude::*;
 use bevy::prelude::*;
 // use bevy_framepace::{FramepaceSettings, Limiter};
 // use bevy_kira_audio::AudioPlugin;
 use bevy_mod_picking::prelude::*;
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+use bevy_rapier3d::prelude::*;
 use bevy_tweening::TweeningPlugin;
 use states::{Loading, MyStates};
-
 pub mod states;
 
 pub struct AppPlugin;
@@ -16,14 +16,21 @@ impl Plugin for AppPlugin {
     fn build(&self, app: &mut App) {
         app.insert_state(MyStates::Load(Loading::Loading));
 
+        let mut config = RapierConfiguration::new(1.);
+        // config.timestep_mode = TimestepMode::Fixed {
+        //     dt: 1. / 60.,
+        //     substeps: 1,
+        // };
         app.add_plugins(PanOrbitCameraPlugin)
             // .add_plugins(AudioPlugin)
             .add_plugins(TweeningPlugin)
             .add_plugins(DefaultPickingPlugins)
-            .add_plugins(PhysicsPlugins::new(PostUpdate))
+            // .add_plugins(PhysicsPlugins::new(PostUpdate))
             // .add_plugins(bevy_framepace::FramepacePlugin)
             // .insert_resource(Time::new_with(Physics::fixed_hz(144.0)))
-            .insert_resource(Time::<Physics>::default().with_relative_speed(2.0))
+            // .insert_resource(Time::<Physics>::default().with_relative_speed(2.0))
+            .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+            .insert_resource(config)
             // .insert_resource(SubstepCount(12))
             ;
 
@@ -43,6 +50,7 @@ impl Plugin for AppPlugin {
         {
             use super::inspector::InspectorPlugin;
             app.add_plugins(InspectorPlugin)
+            .add_plugins(RapierDebugRenderPlugin::default())
                 // .add_plugins(PhysicsDebugPlugin::default())
                 ;
         }
